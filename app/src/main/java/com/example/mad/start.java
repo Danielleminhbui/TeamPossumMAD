5fd77fc94132e36ff25094e254724657743d9d30
danielleminhbui <danielleminhbui@gmail.com>
2020-10-05 14:47:45 +1100

    Finalize setup & login with google auth

---
+++ app/src/main/java/com/example/mad/start.java
@@ -0,0 +1,13 @@
+package com.example.mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
