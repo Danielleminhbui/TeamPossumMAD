2f4da6c833cfbe14743bd9b0941da2368de78f63
danielleminhbui <danielleminhbui@gmail.com>
2020-10-06 23:44:13 +1100

    Location added. User data storage added. 5km radius added

---
+++ app/src/main/java/com/example/mad/setLocation.java
@@ -0,0 +1,13 @@
+package com.example.mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class setLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_location);
+    }
+}
